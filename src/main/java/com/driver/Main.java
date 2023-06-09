f06f4342e0169ffdf7603d8f14fef562e7477a6c
sachin jadhao <jadhaosachin7171@gmail.com>
2023-06-09 12:34:18 +0530

    all code is done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+
+//        Expected 0 arguments but found 1
+
+//        System.out.println(rwOnly.name);
+//        name has private access in 'com.driver.RWOnly'
+
+        rwOnly.setName("sachin");
+        rwOnly.getName();
+    }
 }
